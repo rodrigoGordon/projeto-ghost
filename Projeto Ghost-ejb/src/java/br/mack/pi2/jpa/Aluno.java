b71d2b9316e3e8c03247e7716fd73682ae9a62ad
MichelOh <MichelOh@192.168.1.125>
2014-04-25 12:15:58 -0300

    implementado os campos de persistencia do Aluno e Unidade Academica (parcialmente).

--- Projeto Ghost-ejb/src/java/br/mack/pi2/jpa/Aluno.java
+++ Projeto Ghost-ejb/src/java/br/mack/pi2/jpa/Aluno.java
@@ -7,34 +7,75 @@
 package br.mack.pi2.jpa;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  *
  * @author MichelOh
  */
 @Entity
+@Table (name = "aluno")
 public class Aluno implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "tia")
+    private int TIA;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Unidade_Academica_id_UA", nullable = false)
+    private Unidade_Academica id_UA;
+    @Column (name = "nomeAluno", length = 100)
+    private String nomeAluno;
+    @Column (name = "curso", length = 50)
+    private String curso;
+    @Column (name = "periodo", length = 10)
+    private String periodo;
 
-    public int getId() {
-        return id;
+    public int getTIA() {
+        return TIA;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setTIA(int TIA) {
+        this.TIA = TIA;
     }
 
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+    
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public Unidade_Academica getId_UA() {
+        return id_UA;
+    }
+
+    public void setId_UA(Unidade_Academica id_UA) {
+        this.id_UA = id_UA;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+    
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (int) id;
+        hash += (int) TIA;
         return hash;
     }
 
@@ -45,7 +86,7 @@ public class Aluno implements Serializable {
             return false;
         }
         Aluno other = (Aluno) object;
-        if (this.id != other.id) {
+        if (this.TIA != other.TIA) {
             return false;
         }
         return true;
@@ -53,7 +94,7 @@ public class Aluno implements Serializable {
 
     @Override
     public String toString() {
-        return "br.mack.pi2.jpa.Aluno[ id=" + id + " ]";
+        return "br.mack.pi2.jpa.Aluno[ id=" + TIA + ", nomeAluno =" + nomeAluno + ", curso=" + curso + ", periodo=" + periodo + " ]";
     }
-    
+
 }
--- Projeto Ghost-ejb/src/java/br/mack/pi2/jpa/Unidade_Academica.java
+++ Projeto Ghost-ejb/src/java/br/mack/pi2/jpa/Unidade_Academica.java
@@ -7,10 +7,9 @@
 package br.mack.pi2.jpa;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
 
 /**
  *
@@ -20,32 +19,47 @@ import javax.persistence.Id;
 public class Unidade_Academica implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "id_UA", length = 10, unique = true)
+    private String id_UA;
+    
+    @Column (name = "nome_UA", length = 50)
+    private String nome_UA;
+    
+    @OneToMany (fetch = FetchType.LAZY, mappedBy = "unidade_academica")
+    private Set<Aluno> aluno = new HashSet<Aluno>(0);
 
-    public int getId() {
-        return id;
+    public String getId_UA() {
+        return id_UA;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setId_UA(String id_UA) {
+        this.id_UA = id_UA;
+    }
+    
+    public Set<Aluno> getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Set<Aluno> aluno) {
+        this.aluno = aluno;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (int) id;
+        hash += (id_UA != null ? id_UA.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+        // TODO: Warning - this method won't work in the case the id_UA fields are not set
         if (!(object instanceof Unidade_Academica)) {
             return false;
         }
         Unidade_Academica other = (Unidade_Academica) object;
-        if (this.id != other.id) {
+        if ((this.id_UA == null && other.id_UA != null) || (this.id_UA != null && !this.id_UA.equals(other.id_UA))) {
             return false;
         }
         return true;
@@ -53,7 +67,7 @@ public class Unidade_Academica implements Serializable {
 
     @Override
     public String toString() {
-        return "br.mack.pi2.jpa.Unidade_Academica[ id=" + id + " ]";
+        return "br.mack.pi2.jpa.Unidade_Academica[ id=" + id_UA + " ]";
     }
-    
+
 }
