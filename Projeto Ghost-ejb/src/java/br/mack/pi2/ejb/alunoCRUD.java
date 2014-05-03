aa0fd731cdb875721752e1690193bff91c916ddb
michel.oh <michel.oh@9.78.140.161>
2014-05-03 18:34:37 -0300

    criado a bean de crud do aluno

---
+++ Projeto Ghost-ejb/src/java/br/mack/pi2/ejb/alunoCRUD.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.mack.pi2.ejb;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author michel.oh
+ */
+@Stateless
+@LocalBean
+public class alunoCRUD {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
